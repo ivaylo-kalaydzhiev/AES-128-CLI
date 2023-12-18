14540d5a30cc3d20be99cdb193412e4ce05d7f15
ivaylo-kalaydzhiev <ivailo.s.k@outlook.com>
2023-12-18 14:53:08 +0200

    Create Program class

--- AES-128-CLI/cmake-build-debug/cmake_install.cmake
+++ AES-128-CLI/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/ivaylo.kalaydzhiev/Desktop/AES-128-CLI
+# Install script for directory: /Users/ivaylo.kalaydzhiev/Documents/GitHub/AES-128-CLI/AES-128-CLI
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/ivaylo.kalaydzhiev/Desktop/AES-128-CLI/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/ivaylo.kalaydzhiev/Documents/GitHub/AES-128-CLI/AES-128-CLI/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- AES-128-CLI/main.cpp
+++ AES-128-CLI/main.cpp
@@ -1,6 +1,66 @@
 #include <iostream>
 
+class Program {
+private:
+    static void displayMainMenu() {
+        std::cout << "Chose menu option:" << std::endl
+                  << "(1) Print file contents" << std::endl
+                  << "(2) Encrypt file contents" << std::endl
+                  << "(3) Decrypt file contents" << std::endl
+                  << "(4) Exit Command Line Tool" << std::endl;
+    }
+
+    static int readInt() {
+        int n;
+        std::cin >> n;
+        return n;
+    }
+
+    static void enterPrintFileFlow() {
+        std::cout << "You chose option 1" << std::endl;
+    }
+
+    static void enterEncryptFileFlow() {
+        std::cout << "You chose option 2" << std::endl;
+    }
+
+    static void enterDecryptFileFlow() {
+        std::cout << "You chose option 3" << std::endl;
+    }
+
+    static void exitCLIFlow() {
+        std::cout << "You chose to leave... sad!" << std::endl;
+    }
+
+public:
+    static void enterMainMenuFlow() {
+        displayMainMenu();
+
+        int choice = readInt();
+        switch (choice) {
+            case 1:
+                enterPrintFileFlow();
+                enterMainMenuFlow();
+                break;
+            case 2:
+                enterEncryptFileFlow();
+                enterMainMenuFlow();
+                break;
+            case 3:
+                enterDecryptFileFlow();
+                enterMainMenuFlow();
+                break;
+            case 4:
+                exitCLIFlow();
+                return;
+            default:
+                enterMainMenuFlow();
+        }
+    }
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Program::enterMainMenuFlow();
+
     return 0;
 }
